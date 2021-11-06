dbdb0533f51fd3f07fa0b66a784989b85a673aa1
Alexander <amaslianiy@gmail.com>
2021-11-06 19:36:56 +0300

    Fix bug with redirect to the first page

--- src/hooks/useRequireAuth.ts
+++ src/hooks/useRequireAuth.ts
@@ -1,19 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory, useLocation } from 'react-router';
 import { AppStateType } from '../Redux/store';
 
 export const useRequireAuth = (redirectUrl = '/login') => {
+  const [firstPath, setFirstPath] = useState('/');
   const pathname = useLocation().pathname;
   const authStatus = useSelector(
     (state: AppStateType) => state.user.authStatus
   );
   const history = useHistory();
   useEffect(() => {
-    if (!authStatus) history.push(redirectUrl);
+    if (!authStatus) {
+      if (pathname !== redirectUrl) setFirstPath(pathname);
+      history.push(redirectUrl);
+    }
 
-    if (authStatus && pathname === redirectUrl) history.push('/');
-  }, [authStatus, redirectUrl, history, pathname]);
+    if (authStatus && pathname === redirectUrl) history.push(firstPath);
+  }, [authStatus, redirectUrl, pathname, history, firstPath]);
 
   return authStatus;
 };
