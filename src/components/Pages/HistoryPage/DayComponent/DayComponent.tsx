29b2cd6b9e6b0f28692e807638af3fd1722e6699
Alexander <amaslianiy@gmail.com>
2021-12-15 17:14:28 +0300

    Refactor day component

--- src/components/Pages/HistoryPage/DayComponent/DayComponent.tsx
+++ src/components/Pages/HistoryPage/DayComponent/DayComponent.tsx
@@ -4,35 +4,46 @@ import { format } from 'date-fns';
 import RaitingStarComponent from '../../../../assets/RaitingStarComponent/RaitingStarComponent';
 import { HistoryDay } from '../../../../Redux/modules/historySlice';
 import { LivedTask } from '../../../../Redux/modules/tasksSlice';
+import { FaCouch } from 'react-icons/fa';
 import styles from './DayComponent.module.scss';
+
 const DayComponent: React.FC<PropsType> = ({ day }) => {
   const { doneTasksList, score, timestamp, tag } = day;
+
+  const isDoneTaskList = doneTasksList && doneTasksList.length;
+
+  const date = format(timestamp, 'dd.MM.yyyy');
+
   const mapDoneTaskComponent = (task: LivedTask) => (
     <li className={styles['task-li']} key={task.id}>
       <span>{task.name}</span>
     </li>
   );
-  const date = format(timestamp, 'dd.MM.yyyy');
+
+  const mapStars = (star: string, index: number) => {
+    let scoreValue = index + 1;
+    return (
+      <RaitingStarComponent
+        key={scoreValue}
+        active={score >= scoreValue}
+        size={15}
+      />
+    );
+  };
+
   return (
     <section className={styles.container}>
       <h3 className={styles.date}>{date}</h3>
       <ul className={styles['task-list']}>
-        {doneTasksList &&
-          doneTasksList.length &&
-          doneTasksList.map(mapDoneTaskComponent)}
+        {isDoneTaskList ? (
+          doneTasksList.map(mapDoneTaskComponent)
+        ) : (
+          <FaCouch size={35} />
+        )}
       </ul>
-      <span className={styles.tag}>{tag && <>{tag}</>}</span>
-      <div className={styles.score}>
-        {[...Array(5)].map((star, index) => {
-          let scoreValue = index + 1;
-          return (
-            <RaitingStarComponent
-              key={scoreValue}
-              active={score >= scoreValue}
-              size={15}
-            />
-          );
-        })}
+      <div>
+        <span className={styles.tag}>{tag ?? null}</span>
+        <div className={styles.score}>{[...Array(5)].map(mapStars)}</div>
       </div>
     </section>
   );
